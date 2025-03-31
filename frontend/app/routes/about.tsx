ed7504034e3120bc2b5ff35b0426cbc25e9b5a87
sadikahasan6 <sadikahasan6@gmail.com>
2025-03-31 19:45:17 +0600

    New router trial

---
+++ frontend/app/routes/about.tsx
@@ -0,0 +1,6 @@
+export default function About(){
+    return <div className="m-7 p-7 border bg-purple-100">
+        This is the about page. <br />
+        Don't worry we will finish soon.
+    </div>
+}
---
+++ frontend/app/routes/contact.tsx
@@ -0,0 +1,7 @@
+export default function Contact(){
+    return <div className="m-7 p-7 border bg-blue-100">
+        My name is: Sadik Ahsan <br /> 
+        My email Id: sadikahasan6@gmail.com <br />
+        I am student of BSC Pass course and working as a Merchandiser in a reputed Footwear Manufactuer Industry.
+    </div>
+}
